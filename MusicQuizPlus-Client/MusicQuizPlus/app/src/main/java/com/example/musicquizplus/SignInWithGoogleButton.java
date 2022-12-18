bd541209525e056c2043f04c4c5f21b8da9542d6
CFHoldren <CFHoldren@student.fullsail.edu>
2022-12-18 15:27:24 -0800

    Moved the Google Button into it's own layout

---
+++ MusicQuizPlus-Client/MusicQuizPlus/app/src/main/java/com/example/musicquizplus/SignInWithGoogleButton.java
@@ -0,0 +1,14 @@
+package com.example.musicquizplus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignInWithGoogleButton extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.button_sign_in_with_google);
+    }
+}
