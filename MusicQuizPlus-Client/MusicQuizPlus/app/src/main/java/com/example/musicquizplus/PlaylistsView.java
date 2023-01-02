7f876141096e25f00c2ec55ce0438d3f2e561a97
Matthew Vladika <MJVladika@student.fullsail.edu>
2023-01-02 17:47:26 -0500

    Created and Designed PlaylistsView and GridView Contents

---
+++ MusicQuizPlus-Client/MusicQuizPlus/app/src/main/java/com/example/musicquizplus/PlaylistsView.java
@@ -0,0 +1,14 @@
+package com.example.musicquizplus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlaylistsView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playlists_view);
+    }
+}
