f4d7f31b166f23b3cebb4f6d449fa459973b0db8
Matthew Vladika <MJVladika@student.fullsail.edu>
2023-01-17 16:39:43 -0500

    Created alertdialog with clicking the user avatar as the trigger within ArtistsView

--- MusicQuizPlus-Client/MusicQuizPlus/app/src/main/java/com/example/musicquizplus/ArtistsView.java
+++ MusicQuizPlus-Client/MusicQuizPlus/app/src/main/java/com/example/musicquizplus/ArtistsView.java
@@ -1,12 +1,19 @@
 package com.example.musicquizplus;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AbsListView;
+import android.widget.FrameLayout;
 import android.widget.GridView;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.Objects;
@@ -14,6 +21,8 @@ import java.util.Objects;
 import service.FirebaseService;
 
 public class ArtistsView extends AppCompatActivity {
+
+    private View popupSignUpView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,19 +33,26 @@ public class ArtistsView extends AppCompatActivity {
         TextView userLevel = findViewById(R.id.userLevel);
         View noUser = findViewById(R.id.artistNoCurrentUser);
         ImageButton backToTop = findViewById(R.id.backToTop);
+        View artistsUserAvatar = findViewById(R.id.artistsUserAvatar);
+
+
+        boolean guestAccount;
 
         if(Objects.equals(userLevel.getText(), "GUEST"))
         {
+            guestAccount = true;
             gridView.setVisibility(View.GONE);
             noUser.setVisibility(View.VISIBLE);
         }
         else
         {
+            guestAccount = false;
             gridView.setVisibility(View.VISIBLE);
             noUser.setVisibility(View.GONE);
         }
 
-        if(Objects.equals(gridView.getVisibility(), View.VISIBLE)) {
+        if(!guestAccount)
+        {
             FirebaseService.retrieveData(gridView, this, "sample_artists");
         }
 
@@ -71,5 +87,66 @@ public class ArtistsView extends AppCompatActivity {
             }
         });
 
+        artistsUserAvatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(Objects.equals(userLevel.getText(), "GUEST")) {
+
+                    // Create a AlertDialog Builder.
+                    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(ArtistsView.this);
+                    // Set title, icon, can not cancel properties.
+                    alertDialogBuilder.setTitle("Sign Up for MusicQuizPlus");
+                    alertDialogBuilder.setIcon(null);
+                    alertDialogBuilder.setCancelable(false);
+
+                    // Init popup dialog view and it's ui controls.
+                    popupSignUpView = View.inflate(view.getContext(), R.layout.logged_out_message, null);
+                    // Set the inflated layout view object to the AlertDialog builder.
+                    alertDialogBuilder.setView(popupSignUpView);
+
+                    // Create AlertDialog and show.
+                    final AlertDialog alertDialog = alertDialogBuilder.create();
+                    alertDialog.show();
+
+                    ImageButton cancelPopUp = alertDialog.findViewById(R.id.closeDialogButton);
+                    TextView noThanksLink = alertDialog.findViewById(R.id.noThanksHyperLink);
+                    TextView signUpHeader = alertDialog.findViewById(R.id.logged_out_header);
+                    TextView linkGoogle = alertDialog.findViewById(R.id.link_google);
+                    TextView accountBenefits = alertDialog.findViewById(R.id.account_benefits);
+                    View entireGuestMessage = alertDialog.findViewById(R.id.entireGuestUserMessage);
+
+                    FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, 1300);
+
+                    entireGuestMessage.setLayoutParams(params);
+                    alertDialog.getWindow().setLayout(1000, 1500); //Controlling width and height.
+
+                    noThanksLink.setVisibility(View.VISIBLE);
+                    noThanksLink.setPaintFlags(noThanksLink.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+                    cancelPopUp.setVisibility(View.VISIBLE);
+                    signUpHeader.setTextSize(22);
+                    signUpHeader.setText(R.string.user_profile_signup_header);
+                    linkGoogle.setTextSize(14);
+                    accountBenefits.setTextSize(14);
+
+                    noThanksLink.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            alertDialog.cancel();
+                        }
+                    });
+
+                    cancelPopUp.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            alertDialog.cancel();
+                        }
+                    });
+                }
+                else
+                {
+                    //pull up user profile
+                }
+            }
+        });
     }
 }
