71fcc7794d41ea2133c95cc39dda939a5537a74a
CFHoldren <CFHoldren@student.fullsail.edu>
2023-01-18 11:08:13 -0800

    Created QuizType enum

---
+++ MusicQuizPlus-Client/MusicQuizPlus/app/src/main/java/model/Quiz.java
@@ -0,0 +1,20 @@
+package model;
+
+import com.google.firebase.auth.FirebaseUser;
+
+public class Quiz {
+
+    // Difficulty, level and xp
+    private User user;
+    private FirebaseUser firebaseUser;
+    private
+
+    public Quiz(User user, FirebaseUser firebaseUser) {
+
+    }
+
+    public Quiz() {
+
+    }
+
+}
