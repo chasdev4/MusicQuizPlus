db19cb71c9651351a9ddc6f5f2464f85745738c7
CFHoldren <CFHoldren@student.fullsail.edu>
2023-01-25 11:21:35 -0800

    Removed a question type I added but never used

--- MusicQuizPlus-Client/MusicQuizPlus/app/src/main/java/model/type/QuestionType.java
+++ MusicQuizPlus-Client/MusicQuizPlus/app/src/main/java/model/type/QuestionType.java
@@ -3,7 +3,6 @@ package model.type;
 public enum QuestionType {
     GUESS_TRACK,
     GUESS_ALBUM,
-    GUESS_ARTIST,           // Exclusive to playlist quizzes
+    GUESS_ARTIST,
     GUESS_YEAR,
-    GUESS_FEATURED_ARTIST   // Exclusive to artist quizzes
 }
