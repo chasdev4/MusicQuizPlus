ec9e204cfdb1938b4f1e4c6beae25f3074c7e255
Matthew Vladika <MJVladika@student.fullsail.edu>
2023-02-24 14:21:55 -0500

    Started design on the ArtistQuizView

---
+++ MusicQuizPlus-Client/MusicQuizPlus/app/src/main/java/com/example/musicquizplus/ArtistQuizView.java
@@ -0,0 +1,14 @@
+package com.example.musicquizplus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ArtistQuizView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_artist_quiz_view);
+    }
+}
