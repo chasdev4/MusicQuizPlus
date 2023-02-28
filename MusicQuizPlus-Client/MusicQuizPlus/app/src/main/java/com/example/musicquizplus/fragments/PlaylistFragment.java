f8b7091938063afa14c5f1f96bb3fb549ec7bbe4
Matthew Vladika <MJVladika@student.fullsail.edu>
2023-02-28 16:06:24 -0500

    Displaying user specific playlists, else displaying default playlists

--- MusicQuizPlus-Client/MusicQuizPlus/app/src/main/java/com/example/musicquizplus/fragments/PlaylistFragment.java
+++ MusicQuizPlus-Client/MusicQuizPlus/app/src/main/java/com/example/musicquizplus/fragments/PlaylistFragment.java
@@ -3,6 +3,7 @@ package com.example.musicquizplus.fragments;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -16,10 +17,23 @@ import android.widget.ImageButton;
 
 import com.example.musicquizplus.ParentOfFragments;
 import com.example.musicquizplus.PlaylistQuizView;
+import com.example.musicquizplus.PlaylistsAdapter;
 import com.example.musicquizplus.R;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.CountDownLatch;
 
 import model.GoogleSignIn;
+import model.User;
 import model.item.Playlist;
 import service.FirebaseService;
 
@@ -30,6 +44,9 @@ public class PlaylistFragment extends Fragment {
     private GridView gridView;
     private ImageButton backToTop;
     private View.OnClickListener backToTopListener;
+    private DatabaseReference reference;
+    private List<String> defaultPlaylistIDs = new ArrayList<>();
+    private User user;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -40,17 +57,7 @@ public class PlaylistFragment extends Fragment {
         googleSignIn = new GoogleSignIn();
         firebaseUser = googleSignIn.getAuth().getCurrentUser();
         backToTop = ((ParentOfFragments)getActivity()).findViewById(R.id.backToTop);
-
-        if (firebaseUser != null)
-        {
-            FirebaseService.retrieveData(gridView, getContext(), "playlists", Playlist.class);
-        }
-        else
-        {
-            //TODO: Populate GridView with Default Playlists
-            FirebaseService.retrieveData(gridView, getContext(), "playlists", Playlist.class);
-//            userLevel.setText(getString(R.string.guest));
-        }
+        reference = FirebaseDatabase.getInstance().getReference();
 
         gridView.setOnScrollListener(new AbsListView.OnScrollListener() {
             @Override
@@ -94,6 +101,68 @@ public class PlaylistFragment extends Fragment {
 
         // Inflate the layout for this fragment
         return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        CountDownLatch latch = new CountDownLatch(1);
+
+        new Thread(new Runnable() {
+            public void run() {
+                user = (User) FirebaseService.checkDatabase(reference, "users", firebaseUser.getUid(), User.class);
+                latch.countDown();
+
+                try {
+                    latch.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                CountDownLatch cdl = new CountDownLatch(1);
+
+                if(firebaseUser != null && user.getPlaylistIds().size() != 0)
+                {
+
+                    List<String> playlistIDs = new ArrayList<>(user.getPlaylistIds().values());
+                    FirebaseService.populateGridViewByPlaylistIDs(reference, getActivity(), getContext(), gridView, playlistIDs);
+
+                }
+                else
+                {
+                    new Thread(new Runnable() {
+                        public void run() {
+                            reference.child("default_playlists").addValueEventListener(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                                        defaultPlaylistIDs.add(dataSnapshot.getValue(String.class));
+                                    }
+                                    cdl.countDown();
+                                }
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError error) {
+
+                                }
+                            });
+
+                            try {
+                                cdl.await();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+
+                            if(defaultPlaylistIDs.size() > 0)
+                            {
+                                FirebaseService.populateGridViewByPlaylistIDs(reference, getActivity(), getContext(), gridView, defaultPlaylistIDs);
+                            }
+
+                        }
+                    }).start();
+                }
+            }
+        }).start();
     }
 
     public void createBackToTopListener() {
